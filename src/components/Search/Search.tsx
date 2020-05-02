ee45639e6ae9ce219c5ac405188d24a45e2a68d5
Jevgeni Rumjantsev <zeka.rum@gmail.com>
2020-05-02 18:01:19 +0200

    fixed a bug with momentjs; added prefill button; added two more destinations

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 import moment from 'moment';
 
-import AccuWeatherApi from '../apis/accuWeatherApi';
-import KiwiApi from '../apis/kiwiApi';
+import AccuWeatherApi from '../../apis/accuWeatherApi';
+import KiwiApi from '../../apis/kiwiApi';
 
 const destinations = [
   {cityName: 'Amsterdam', countryID: 'NL', iataCode: 'AMS'},
-  // {cityName: 'Budapest', countryID: 'HU', iataCode: 'BUD'},
-  // {cityName: 'Madrid', countryID: 'ES', iataCode: 'MAD'},
+  {cityName: 'Budapest', countryID: 'HU', iataCode: 'BUD'},
+  {cityName: 'Madrid', countryID: 'ES', iataCode: 'MAD'},
 ];
 
 export default function Search() {
@@ -15,6 +15,17 @@ export default function Search() {
   const [dateFrom, setDateFrom] = useState('');
   const [dateTo, setDateTo] = useState('');
   const [isSearching, setIsSearching] = useState(false);
+
+  const handlePrefill = async () => {
+    setFlyingFrom("LON");
+    setDateFrom(moment()
+      .add(6, "months")
+      .format('YYYY-MM-DD'));
+    setDateTo(moment()
+      .add(6, "months")
+      .add(10, "days")
+      .format('YYYY-MM-DD'));
+  };
 
   const handleSearch = async () => {
     const promises = [];
@@ -26,8 +37,7 @@ export default function Search() {
     for (let destination of destinations) {
       const accuWeatherPromise = accuWeatherApi.citySearch(destination.cityName)
         .then(response => response.find(item => item.Country.ID === destination.countryID))
-        .then(foundItem => accuWeatherApi.get1DayForecast(foundItem!.Key))
-        .then(forecast => console.log(forecast));
+        .then(foundItem => accuWeatherApi.get1DayForecast(foundItem!.Key));
 
       const kiwiPromise = kiwiApi.searchFlights({
         dateFrom: moment(dateFrom),
@@ -53,6 +63,8 @@ export default function Search() {
   return (
     <div>
       <h3>Where are you flying from?</h3>
+      <button onClick={handlePrefill}>Prefill fields please</button>
+      <hr/>
       <input type="text" id="flying-from" value={flyingFrom} onChange={e => setFlyingFrom(e.target.value)} />
       <input type="date" id="flying-date-from" value={dateFrom} onChange={e => setDateFrom(e.target.value)} />
       <input type="date" id="flying-date-to" value={dateTo} onChange={e => setDateTo(e.target.value)} />
