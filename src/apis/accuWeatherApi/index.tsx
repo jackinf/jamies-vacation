6ff6fdad3625323474227000b58e5e8390af3463
Jevgeni Rumjantsev <zeka.rum@gmail.com>
2020-05-03 09:56:04 +0200

    fixed HTTP -> HTTPS calls for external APIs if the app is hosted on HTTPS

--- src/apis/accuWeatherApi/index.tsx
+++ src/apis/accuWeatherApi/index.tsx
@@ -4,8 +4,10 @@ import { CitySearchResponseItem, NDayForecastResponse } from './types';
 const apiKey = config.apiKeys.accuWeatherApiKey;
 
 export default class AccuWeatherApi {
+  baseUrl = 'https://dataservice.accuweather.com';
+
   public async citySearch(destinationCity: string): Promise<CitySearchResponseItem[]> {
-    const url = new URL('http://dataservice.accuweather.com/locations/v1/cities/search');
+    const url = new URL(`${this.baseUrl}/locations/v1/cities/search`);
 
     const params = new URLSearchParams();
     params.append('apikey', apiKey);
@@ -17,7 +19,7 @@ export default class AccuWeatherApi {
   };
 
   public async get1DayForecast(locationKey: string): Promise<NDayForecastResponse> {
-    const url = new URL(`http://dataservice.accuweather.com/forecasts/v1/daily/1day/${locationKey}`);
+    const url = new URL(`${this.baseUrl}/forecasts/v1/daily/1day/${locationKey}`);
     const params = new URLSearchParams();
     params.append('apikey', apiKey);
     const response = await fetch(`${url}?${params}`);
